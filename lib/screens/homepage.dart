9d59386c88055b7b92b8b06f7cf25baa3a0cf7ec
hyungmuk <55679057+hyungmuk@users.noreply.github.com>
2022-01-16 02:24:18 +0900

    Flutter - Todo App from Scratch - Part 1

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:what_todo/screens/widgets.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+          color: Color(0xFFF6F6F6),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 32.0),
+                    child: Image(image: AssetImage('assets/images/logo.png')),
+                  ),
+                  TaskCardWidget(
+                    title: "Get Started!",
+                    desc:
+                        "Hello User!, Welcome to WHAT_TODO app. this is a default task that you can edit or delete to start using the app.",
+                  ),
+                  TaskCardWidget(),
+                ],
+              ),
+              Positioned(
+                width: 60.0,
+                height: 60.0,
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF7349FE),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/add_icon.png"
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
